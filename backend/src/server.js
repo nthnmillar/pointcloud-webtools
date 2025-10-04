7247f75b307b1ed540ee30c38f621b98b2e985d1
Nathan Millar <nthnmillar@gmail.com>
2025-10-05 09:57:18 +1000

    improved ports on startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 import VoxelDownsampler from './services/tools/VoxelDownsampler.js';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Initialize services
 const voxelDownsampler = new VoxelDownsampler();
