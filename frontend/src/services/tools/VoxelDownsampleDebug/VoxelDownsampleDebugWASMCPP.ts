8c62d36ba21e59fb18746192b7f84ab38f589431
Nathan Millar <nthnmillar@gmail.com>
2025-12-14 11:52:00 +1000

    ts ingore line

--- frontend/src/services/tools/VoxelDownsampleDebug/VoxelDownsampleDebugWASMCPP.ts
+++ frontend/src/services/tools/VoxelDownsampleDebug/VoxelDownsampleDebugWASMCPP.ts
@@ -85,10 +85,8 @@ export class VoxelDownsampleDebugWASMCPP extends BaseService {
         'VoxelDownsampleDebugWASMCPP',
         'window.ToolsModule not found, attempting dynamic import'
       );
-      // @ts-ignore - TypeScript cannot resolve relative path module declarations for WASM files
-      const ToolsModuleNs = (await import(
-        '../../../../public/wasm/cpp/tools_cpp.js'
-      )) as {
+      // @ts-expect-error - TypeScript cannot resolve relative path module declarations for WASM files
+      const ToolsModuleNs = (await import('../../../../public/wasm/cpp/tools_cpp.js')) as {
         default?: (options?: {
           locateFile?: (path: string) => string;
         }) => Promise<ToolsWasmModule>;
